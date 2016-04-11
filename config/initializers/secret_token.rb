422199099d4dd859f5e52ac985478bda7d5c453f
To <vinh.to@P35965.local>
2016-04-11 22:56:38 +0900

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestI18njs::Application.config.secret_token = 'fde4f57344ce628ae973e420e78125cf5e1b38c39fc63cb45350cebf4e4238c4a8b78f67bae73802d10238310546098dd776f3c7d4e4ee1aa1a94ec75b53fdcf'
